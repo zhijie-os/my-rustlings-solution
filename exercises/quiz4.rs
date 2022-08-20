2711b8473d9680650fb226becddf4ef4e2745998
Zhijie Xia <zhijiexiacs@gmail.com>
2022-08-19 19:38:04 -0600

    Macros + quiz4

--- exercises/quiz4.rs
+++ exercises/quiz4.rs
@@ -5,7 +5,13 @@
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
 
-// I AM NOT DONE
+
+#[macro_export]
+macro_rules! my_macro {
+    ($x:expr) => {
+        String::from("Hello ") + $x
+    };
+}
 
 #[cfg(test)]
 mod tests {
